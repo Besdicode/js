4c649f5237719abeefe938ae752745badfcfd8a4
Besdicode <besdiseyidovaa@gmail.com>
2023-05-09 15:36:16 +0300

    js

---
+++ indexx.js
@@ -0,0 +1,35 @@
+// tapshiriq1
+let kmhSpeed = 75 
+msSpeed = kmhSpeed*10/36
+console.log(msSpeed);
+
+
+// tapshiriq2
+let fullName = "Alan Turing"
+console.log(fullName.length -1); 
+
+
+// tapshiriq3
+let fullNamee = "Besdi Seyidova"
+console.log(fullNamee[0] + fullNamee[6]); 
+
+
+// tapshiriq5
+let number = prompt("iki eded daxil edin")
+let numbeer = prompt("iki eded daxil edin")
+console.log(number*numbeer);
+console.log(number+numbeer);
+console.log(number-numbeer);
+console.log(number/numbeer);  
+
+
+// tapshiriq6
+let num = 12345 * 98765
+let num1 = String(num)
+console.log(num1[6]);       
+
+
+// tapshiriq8
+let reqem = prompt("530.90 daxil edin")
+let reqemm = ('530 manat 30 qepik')
+console.log(reqemm);
